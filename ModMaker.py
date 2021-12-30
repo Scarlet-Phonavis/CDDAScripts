145df48453f90f80806a77cb9175e87b47da73e9
Scarlet-Phonavis <unstablegamers109@gmail.com>
2021-12-30 03:45:57 -0600

    Leaving this here in case I delete it or lose it

---
+++ ModMaker.py
@@ -0,0 +1,41 @@
+import json
+import os
+import sys
+
+global exportlist
+exportlist = []
+os.makedirs("export", exist_ok=True)
+
+
+
+MUTATIONINPUT = sys.argv[1]
+EXPORTFILE = "export/" + sys.argv[2]
+
+
+READINPUT = open(MUTATIONINPUT, mode="r")
+WRITEOUTPUT = open(EXPORTFILE, mode='w+')
+
+
+JSONINPUT = json.load(READINPUT)
+
+for x in JSONINPUT:
+    currentthresh = x.get("threshreq")
+    thresholdtrue = x.get("threshold")
+    if currentthresh != None or thresholdtrue != None:
+        threshentry = {}
+        threshentry["type"] = x.get("type")
+        threshentry["id"] = x.get("id")
+        threshentry["copy-from"] = x.get("id")
+        threshentry["threshreq"] = []
+        threshentry["threshold"] = False
+        exportlist.append(threshentry)
+
+
+
+if len(exportlist) == 0:
+    print("No threshold mutations detected in [0]".format(MUTATIONINPUT))
+else:
+    json.dump(exportlist, WRITEOUTPUT, indent = 4)
+        
+
+
