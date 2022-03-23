60aad13e9a90126a64dc027b319c363bf7224f1f
Scarlet-Phonavis <unstablegamers109@gmail.com>
2022-03-23 06:39:55 -0500

    Add files via upload

---
+++ catapult_mod_cleaner/readme.txt
@@ -0,0 +1,2 @@
+Place in same folder as "mod_repo" and "current"
+Will delete all mods in "current/data/mods" that exist in "mod_repo" for easy mod updating 
---
+++ mutation_thresh_remover/SingleModMaker.py
@@ -0,0 +1,48 @@
+import json
+import os
+import sys
+
+global exportlist
+exportlist = []
+os.makedirs("export", exist_ok=True)
+
+MUTATIONINPUT = sys.argv[1]
+
+
+if len(sys.argv) == 3:
+	EXPORTFOLDER = "export/" + sys.argv[2]
+elif len(sys.argv) == 2:
+	EXPORTFOLDER = "export/" + sys.argv[1]
+else:
+	print("Additonal Paramters In Entry")
+
+
+READINPUT = open(MUTATIONINPUT, mode="r")
+WRITEOUTPUT = open(EXPORTFOLDER, mode='w+')
+
+
+JSONINPUT = json.load(READINPUT)
+
+for x in JSONINPUT:
+    currentthresh = x.get("threshreq")
+    thresholdtrue = x.get("threshold")
+    if currentthresh != None or thresholdtrue != None:
+        threshentry = {}
+        threshentry["type"] = x.get("type")
+        threshentry["id"] = x.get("id")
+        threshentry["copy-from"] = x.get("id")
+        threshentry["threshreq"] = []
+        threshentry["threshold"] = False
+        exportlist.append(threshentry)
+
+
+
+if len(exportlist) == 0:
+    print("No threshold mutations detected in [0]".format(MUTATIONINPUT))
+else:
+    json.dump(exportlist, WRITEOUTPUT, indent = 4)
+        
+
+WRITEOUTPUT.close()
+READINPUT.close()
+
